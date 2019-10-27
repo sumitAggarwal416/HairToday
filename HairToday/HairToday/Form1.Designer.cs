a63c3d0587deb906d3dfe710026be7e7ff6b1b29
Sumit Aggarwal <55593642+sumitAggarwal416@users.noreply.github.com>
2019-10-26 22:08:28 -0400

    Add files via upload

---
+++ HairToday/HairToday/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace HairToday
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hairdresserGroupBox = new System.Windows.Forms.GroupBox();
+            this.lauraRenkinsRadioButton = new System.Windows.Forms.RadioButton();
+            this.suePallonRadioButton = new System.Windows.Forms.RadioButton();
+            this.ronChambersRadioButton = new System.Windows.Forms.RadioButton();
+            this.patJohnsonRadioButton = new System.Windows.Forms.RadioButton();
+            this.janeSamleyRadioButton = new System.Windows.Forms.RadioButton();
+            this.servicesGroupBox = new System.Windows.Forms.GroupBox();
+            this.extensionsCheckBox = new System.Windows.Forms.CheckBox();
+            this.highlightsCheckBox = new System.Windows.Forms.CheckBox();
+            this.colorCheckBox = new System.Windows.Forms.CheckBox();
+            this.cutCheckBox = new System.Windows.Forms.CheckBox();
+            this.clientTypeGroupBox = new System.Windows.Forms.GroupBox();
+            this.standardAdultRadioButton = new System.Windows.Forms.RadioButton();
+            this.childRadioButton = new System.Windows.Forms.RadioButton();
+            this.studentRadioButton = new System.Windows.Forms.RadioButton();
+            this.seniorRadioButton = new System.Windows.Forms.RadioButton();
+            this.clientVisitsGroupBox = new System.Windows.Forms.GroupBox();
+            this.clientVisitsTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
+            this.hairdresserGroupBox.SuspendLayout();
+            this.servicesGroupBox.SuspendLayout();
+            this.clientTypeGroupBox.SuspendLayout();
+            this.clientVisitsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hairdresserGroupBox
+            // 
+            this.hairdresserGroupBox.Controls.Add(this.lauraRenkinsRadioButton);
+            this.hairdresserGroupBox.Controls.Add(this.suePallonRadioButton);
+            this.hairdresserGroupBox.Controls.Add(this.ronChambersRadioButton);
+            this.hairdresserGroupBox.Controls.Add(this.patJohnsonRadioButton);
+            this.hairdresserGroupBox.Controls.Add(this.janeSamleyRadioButton);
+            this.hairdresserGroupBox.Location = new System.Drawing.Point(68, 22);
+            this.hairdresserGroupBox.Name = "hairdresserGroupBox";
+            this.hairdresserGroupBox.Size = new System.Drawing.Size(310, 317);
+            this.hairdresserGroupBox.TabIndex = 0;
+            this.hairdresserGroupBox.TabStop = false;
+            this.hairdresserGroupBox.Text = "Hairdresser";
+            // 
+            // lauraRenkinsRadioButton
+            // 
+            this.lauraRenkinsRadioButton.AutoSize = true;
+            this.lauraRenkinsRadioButton.Location = new System.Drawing.Point(43, 267);
+            this.lauraRenkinsRadioButton.Name = "lauraRenkinsRadioButton";
+            this.lauraRenkinsRadioButton.Size = new System.Drawing.Size(121, 21);
+            this.lauraRenkinsRadioButton.TabIndex = 4;
+            this.lauraRenkinsRadioButton.Text = "Laura Renkins";
+            this.lauraRenkinsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // suePallonRadioButton
+            // 
+            this.suePallonRadioButton.AutoSize = true;
+            this.suePallonRadioButton.Location = new System.Drawing.Point(43, 212);
+            this.suePallonRadioButton.Name = "suePallonRadioButton";
+            this.suePallonRadioButton.Size = new System.Drawing.Size(97, 21);
+            this.suePallonRadioButton.TabIndex = 3;
+            this.suePallonRadioButton.Text = "Sue Pallon";
+            this.suePallonRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ronChambersRadioButton
+            // 
+            this.ronChambersRadioButton.AutoSize = true;
+            this.ronChambersRadioButton.Location = new System.Drawing.Point(43, 157);
+            this.ronChambersRadioButton.Name = "ronChambersRadioButton";
+            this.ronChambersRadioButton.Size = new System.Drawing.Size(123, 21);
+            this.ronChambersRadioButton.TabIndex = 2;
+            this.ronChambersRadioButton.Text = "Ron Chambers";
+            this.ronChambersRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // patJohnsonRadioButton
+            // 
+            this.patJohnsonRadioButton.AutoSize = true;
+            this.patJohnsonRadioButton.Location = new System.Drawing.Point(43, 106);
+            this.patJohnsonRadioButton.Name = "patJohnsonRadioButton";
+            this.patJohnsonRadioButton.Size = new System.Drawing.Size(108, 21);
+            this.patJohnsonRadioButton.TabIndex = 1;
+            this.patJohnsonRadioButton.Text = "Pat Johnson";
+            this.patJohnsonRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // janeSamleyRadioButton
+            // 
+            this.janeSamleyRadioButton.AutoSize = true;
+            this.janeSamleyRadioButton.Checked = true;
+            this.janeSamleyRadioButton.Location = new System.Drawing.Point(43, 52);
+            this.janeSamleyRadioButton.Name = "janeSamleyRadioButton";
+            this.janeSamleyRadioButton.Size = new System.Drawing.Size(110, 21);
+            this.janeSamleyRadioButton.TabIndex = 0;
+            this.janeSamleyRadioButton.TabStop = true;
+            this.janeSamleyRadioButton.Text = "Jane Samley";
+            this.janeSamleyRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // servicesGroupBox
+            // 
+            this.servicesGroupBox.Controls.Add(this.extensionsCheckBox);
+            this.servicesGroupBox.Controls.Add(this.highlightsCheckBox);
+            this.servicesGroupBox.Controls.Add(this.colorCheckBox);
+            this.servicesGroupBox.Controls.Add(this.cutCheckBox);
+            this.servicesGroupBox.Location = new System.Drawing.Point(464, 22);
+            this.servicesGroupBox.Name = "servicesGroupBox";
+            this.servicesGroupBox.Size = new System.Drawing.Size(310, 317);
+            this.servicesGroupBox.TabIndex = 1;
+            this.servicesGroupBox.TabStop = false;
+            this.servicesGroupBox.Text = "Services";
+            // 
+            // extensionsCheckBox
+            // 
+            this.extensionsCheckBox.AutoSize = true;
+            this.extensionsCheckBox.Location = new System.Drawing.Point(61, 212);
+            this.extensionsCheckBox.Name = "extensionsCheckBox";
+            this.extensionsCheckBox.Size = new System.Drawing.Size(98, 21);
+            this.extensionsCheckBox.TabIndex = 3;
+            this.extensionsCheckBox.Text = "Extensions";
+            this.extensionsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // highlightsCheckBox
+            // 
+            this.highlightsCheckBox.AutoSize = true;
+            this.highlightsCheckBox.Location = new System.Drawing.Point(61, 157);
+            this.highlightsCheckBox.Name = "highlightsCheckBox";
+            this.highlightsCheckBox.Size = new System.Drawing.Size(92, 21);
+            this.highlightsCheckBox.TabIndex = 2;
+            this.highlightsCheckBox.Text = "Highlights";
+            this.highlightsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // colorCheckBox
+            // 
+            this.colorCheckBox.AutoSize = true;
+            this.colorCheckBox.Location = new System.Drawing.Point(61, 106);
+            this.colorCheckBox.Name = "colorCheckBox";
+            this.colorCheckBox.Size = new System.Drawing.Size(63, 21);
+            this.colorCheckBox.TabIndex = 1;
+            this.colorCheckBox.Text = "Color";
+            this.colorCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cutCheckBox
+            // 
+            this.cutCheckBox.AutoSize = true;
+            this.cutCheckBox.Location = new System.Drawing.Point(61, 52);
+            this.cutCheckBox.Name = "cutCheckBox";
+            this.cutCheckBox.Size = new System.Drawing.Size(51, 21);
+            this.cutCheckBox.TabIndex = 0;
+            this.cutCheckBox.Text = "Cut";
+            this.cutCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // clientTypeGroupBox
+            // 
+            this.clientTypeGroupBox.Controls.Add(this.standardAdultRadioButton);
+            this.clientTypeGroupBox.Controls.Add(this.childRadioButton);
+            this.clientTypeGroupBox.Controls.Add(this.studentRadioButton);
+            this.clientTypeGroupBox.Controls.Add(this.seniorRadioButton);
+            this.clientTypeGroupBox.Location = new System.Drawing.Point(68, 355);
+            this.clientTypeGroupBox.Name = "clientTypeGroupBox";
+            this.clientTypeGroupBox.Size = new System.Drawing.Size(310, 317);
+            this.clientTypeGroupBox.TabIndex = 1;
+            this.clientTypeGroupBox.TabStop = false;
+            this.clientTypeGroupBox.Text = "Client Type";
+            // 
+            // standardAdultRadioButton
+            // 
+            this.standardAdultRadioButton.AutoSize = true;
+            this.standardAdultRadioButton.Checked = true;
+            this.standardAdultRadioButton.Location = new System.Drawing.Point(43, 44);
+            this.standardAdultRadioButton.Name = "standardAdultRadioButton";
+            this.standardAdultRadioButton.Size = new System.Drawing.Size(123, 21);
+            this.standardAdultRadioButton.TabIndex = 5;
+            this.standardAdultRadioButton.TabStop = true;
+            this.standardAdultRadioButton.Text = "Standard Adult";
+            this.standardAdultRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // childRadioButton
+            // 
+            this.childRadioButton.AutoSize = true;
+            this.childRadioButton.Location = new System.Drawing.Point(43, 115);
+            this.childRadioButton.Name = "childRadioButton";
+            this.childRadioButton.Size = new System.Drawing.Size(159, 21);
+            this.childRadioButton.TabIndex = 6;
+            this.childRadioButton.Text = "Child (12 and under)";
+            this.childRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // studentRadioButton
+            // 
+            this.studentRadioButton.AutoSize = true;
+            this.studentRadioButton.Location = new System.Drawing.Point(43, 192);
+            this.studentRadioButton.Name = "studentRadioButton";
+            this.studentRadioButton.Size = new System.Drawing.Size(78, 21);
+            this.studentRadioButton.TabIndex = 7;
+            this.studentRadioButton.Text = "Student";
+            this.studentRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // seniorRadioButton
+            // 
+            this.seniorRadioButton.AutoSize = true;
+            this.seniorRadioButton.Location = new System.Drawing.Point(43, 274);
+            this.seniorRadioButton.Name = "seniorRadioButton";
+            this.seniorRadioButton.Size = new System.Drawing.Size(143, 21);
+            this.seniorRadioButton.TabIndex = 8;
+            this.seniorRadioButton.Text = "Senior (above 65)";
+            this.seniorRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // clientVisitsGroupBox
+            // 
+            this.clientVisitsGroupBox.Controls.Add(this.clientVisitsTextBox);
+            this.clientVisitsGroupBox.Controls.Add(this.label1);
+            this.clientVisitsGroupBox.Location = new System.Drawing.Point(464, 355);
+            this.clientVisitsGroupBox.Name = "clientVisitsGroupBox";
+            this.clientVisitsGroupBox.Size = new System.Drawing.Size(310, 317);
+            this.clientVisitsGroupBox.TabIndex = 1;
+            this.clientVisitsGroupBox.TabStop = false;
+            this.clientVisitsGroupBox.Text = "Client Visits";
+            // 
+            // clientVisitsTextBox
+            // 
+            this.clientVisitsTextBox.Location = new System.Drawing.Point(43, 88);
+            this.clientVisitsTextBox.Name = "clientVisitsTextBox";
+            this.clientVisitsTextBox.Size = new System.Drawing.Size(229, 22);
+            this.clientVisitsTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Client Visits";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 695);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Total Price:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(130, 727);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(99, 44);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(345, 727);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(94, 44);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(556, 727);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(99, 44);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // totalPriceLabel
+            // 
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Location = new System.Drawing.Point(208, 695);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(0, 17);
+            this.totalPriceLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 783);
+            this.Controls.Add(this.totalPriceLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.servicesGroupBox);
+            this.Controls.Add(this.clientTypeGroupBox);
+            this.Controls.Add(this.clientVisitsGroupBox);
+            this.Controls.Add(this.hairdresserGroupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.hairdresserGroupBox.ResumeLayout(false);
+            this.hairdresserGroupBox.PerformLayout();
+            this.servicesGroupBox.ResumeLayout(false);
+            this.servicesGroupBox.PerformLayout();
+            this.clientTypeGroupBox.ResumeLayout(false);
+            this.clientTypeGroupBox.PerformLayout();
+            this.clientVisitsGroupBox.ResumeLayout(false);
+            this.clientVisitsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox hairdresserGroupBox;
+        private System.Windows.Forms.RadioButton lauraRenkinsRadioButton;
+        private System.Windows.Forms.RadioButton suePallonRadioButton;
+        private System.Windows.Forms.RadioButton ronChambersRadioButton;
+        private System.Windows.Forms.RadioButton patJohnsonRadioButton;
+        private System.Windows.Forms.RadioButton janeSamleyRadioButton;
+        private System.Windows.Forms.GroupBox servicesGroupBox;
+        private System.Windows.Forms.CheckBox extensionsCheckBox;
+        private System.Windows.Forms.CheckBox highlightsCheckBox;
+        private System.Windows.Forms.CheckBox colorCheckBox;
+        private System.Windows.Forms.CheckBox cutCheckBox;
+        private System.Windows.Forms.GroupBox clientTypeGroupBox;
+        private System.Windows.Forms.RadioButton standardAdultRadioButton;
+        private System.Windows.Forms.RadioButton childRadioButton;
+        private System.Windows.Forms.RadioButton studentRadioButton;
+        private System.Windows.Forms.RadioButton seniorRadioButton;
+        private System.Windows.Forms.GroupBox clientVisitsGroupBox;
+        private System.Windows.Forms.TextBox clientVisitsTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label totalPriceLabel;
+    }
+}
+
---
+++ HairToday/HairToday/Form1.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * @author Sumit Aggarwal - 000793607
+ * I certify that all work done in the assignment is my own work and that I have not copied it
+ * from any other source. I also certify that I have not allowed anyone else to copy my code.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HairToday
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// calculates the total amount of the services according to which hairdresser you chose, what kind of services you want
+        /// how many times have you visited the place and whether you are an adult, a child, a student or a senior
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double price = 0;
+                int visits = int.Parse(clientVisitsTextBox.Text);
+                Boolean isChecked = cutCheckBox.Checked || colorCheckBox.Checked || highlightsCheckBox.Checked ||
+                    extensionsCheckBox.Checked;
+
+                if (janeSamleyRadioButton.Checked)
+                    price += 30;
+                else if (patJohnsonRadioButton.Checked)
+                    price += 45;
+                else if (ronChambersRadioButton.Checked)
+                    price += 40;
+                else if (suePallonRadioButton.Checked)
+                    price += 50;
+                else if (lauraRenkinsRadioButton.Checked)
+                    price += 55;
+
+                if (cutCheckBox.Checked)
+                    price += 30;
+                if (colorCheckBox.Checked)
+                    price += 40;
+                if (highlightsCheckBox.Checked)
+                    price += 50;
+                if (extensionsCheckBox.Checked)
+                    price += 200;
+
+                if (standardAdultRadioButton.Checked)
+                    price -= 0;
+                else if (childRadioButton.Checked)
+                    price -= 0.1 * price;
+                else if (studentRadioButton.Checked)
+                    price -= 0.05 * price;
+                else if (seniorRadioButton.Checked)
+                    price -= 0.15 * price;
+
+                if (visits > 0 && visits < 4)
+                    price -= 0;
+                else if (visits >= 4 && visits <= 8)
+                    price -= 0.05 * price;
+                else if (visits >= 9 && visits <= 13)
+                    price -= 0.1 * price;
+                else if (visits >= 14)
+                    price -= 0.15 * price;
+
+                if (visits < 0 || isChecked == false)
+                    totalPriceLabel.Text = "No service selected! Select a service and then try again.";
+                else
+                    totalPriceLabel.Text = "$" + price;
+            }
+            catch(Exception ex)
+            {
+                totalPriceLabel.Text = "Invalid entry!" + ex.Message;
+            }
+            
+        }
+        /// <summary>
+        /// closes the form when clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// clears the form and sends the focus to the janeSamleyRadioButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            if (cutCheckBox.Checked)
+                cutCheckBox.Checked = false;
+            if (colorCheckBox.Checked)
+                colorCheckBox.Checked = false;
+            if (highlightsCheckBox.Checked)
+                highlightsCheckBox.Checked = false;
+            if (extensionsCheckBox.Checked)
+                extensionsCheckBox.Checked = false;
+
+            janeSamleyRadioButton.Checked = true;
+            standardAdultRadioButton.Checked = true;
+
+            clientVisitsTextBox.Text = "";
+
+            janeSamleyRadioButton.Focus();
+
+            totalPriceLabel.Text = "";
+        }
+    }
+}
